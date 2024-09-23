52f112f5e47076ad479b9da9ab989ec92ec4e504
kyupin64 <lewilks.321@gmail.com>
2024-09-23 00:24:27 -0600

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MaterialModule } from './modules/material.module';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './modules/app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { DepartmentsComponent } from './components/departments/departments.component';
+import { TimesheetComponent } from './components/timesheet/timesheet.component';
+import { AnalyticsComponent } from './components/analytics/analytics.component';
+import { TopNavbarComponent } from './components/top-navbar/top-navbar.component';
+import { AnalyticsTableComponent } from './components/analytics-table/analytics-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepartmentsComponent,
+    TimesheetComponent,
+    AnalyticsComponent,
+    TopNavbarComponent,
+    AnalyticsTableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
